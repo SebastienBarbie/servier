e3af4f8ed64c34fb219f6df8947bb26f1974fca8
Sébastien BARBIER <79325955+SebastienBarbie@users.noreply.github.com>
2023-02-13 15:33:01 +0100

    adding of the app within the mainfile

--- main.py
+++ main.py
@@ -1,42 +1,62 @@
 from model import ModelFromScratch
 from dataset import Dataset
 import os
-import sys
-import tensorflow as tf
-import keras
+from tkinter.filedialog import askopenfilename
+from tensorflow.keras.preprocessing import image
+import numpy as np
 
 def main():
 
-    value = 0
-    while value < 1 or value > 3:
-        value = input("1 - Train the model\n2 - Evaluate the model\n3 - Predict the stage of the disease\n")
+    value = 1
+    while value >= 1 and value <= 3:
+        value = input("1 - Train the model\n2 - Evaluate the model\n3 - Predict the stage of the disease\n4 - Exit the programm\n")
         if value.isdigit():
             value = int(value)
         else:
             value = 0
 
-    
-    if value == 1 :
-        epochs = input("How many epochs ?\n")
-        if epochs.isdigit():
-            epochs = int(epochs)
-        else:
-            epochs = 10
-
-        model = ModelFromScratch()
         path = 'd:/servier/Test_Technique_Image/Neuroflux_disorder/'
         classes = os.listdir(path)
+        model = ModelFromScratch()
 
-        train = Dataset(path, classes, "training", 0.2)
-        train.preprocess()
-        train.generate_coeffs()
+        if value == 1 :
+            epochs = input("How many epochs ?\n")
+            if epochs.isdigit():
+                epochs = int(epochs)
+            else:
+                epochs = 10
 
-        print(train.coeffs)
+            
+        
+            classes = os.listdir(path)
 
-        val = Dataset(path, classes, "validation", 0.2)
+            train = Dataset(path, classes, "training", 0.2)
+            train.preprocess()
+            train.generate_coeffs()
 
-        model.fit(train.get_datas(), val.get_datas(), epochs, train.coeffs)
-    
+            print(train.coeffs)
+
+            val = Dataset(path, classes, "validation", 0.2)
+
+            model.fit(train.get_datas(), val.get_datas(), epochs, train.coeffs)
+
+        if value == 2:
+            train = Dataset(path, classes, "training", 0.2)
+            train.preprocess()
+            train.generate_coeffs()
+            model = ModelFromScratch()
+            model.evaluate(train.get_datas())
+
+        if value == 3: 
+            classes = os.listdir(path)
+            FILETYPES = [ ("jpg files", "*.jpg") ]
+            file = askopenfilename(filetypes=FILETYPES)
+            img = image.load_img(file, target_size=(32, 32))
+            img_array = image.img_to_array(img)
+            img_batch = np.expand_dims(img_array, axis=0)
+            predict = model.predict(img_batch)
+            print(classes[predict.argmax()])
+
 
 
 
--- model.py
+++ model.py
@@ -60,20 +60,21 @@ class ModelFromScratch():
 
         self._model = model
 
-    # def compile(self):
-    #     self._model.compile(loss='categorical_crossentropy', optimizer=self.opt, metrics=['accuracy', 'Recall'])
+    def compile(self):
+        self._model.compile(loss='categorical_crossentropy', optimizer=self.opt, metrics=['accuracy', 'Recall'])
 
     def fit(self, train, val, epochs, coeff):
         self._model.compile(loss='categorical_crossentropy', optimizer=self.opt, metrics=['accuracy', 'Recall'])
         mcp_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='min')
 
-        earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
-        reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, min_delta=1e-4, mode='min')
-        fit = self._model.fit(train,epochs=epochs, validation_data=val)#, class_weight = coeff ,callbacks=[mcp_save, reduce_lr_loss])
+        # earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
+        # reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, min_delta=1e-4, mode='min')
+        fit = self._model.fit(train,epochs=epochs, validation_data=val, class_weight = coeff)# ,callbacks=[mcp_save, reduce_lr_loss])
         self._model.save('./model')
 
     
     def evaluate(self, ds):
+        self._model.compile(loss='categorical_crossentropy', optimizer=self.opt, metrics=['accuracy', 'Recall'])
         self._model.evaluate(ds)
     
     def get_model(self):
@@ -81,4 +82,7 @@ class ModelFromScratch():
 
     def save(self):
         self._model.save(os.absolutpath('./model'))
+
+    def predict(self, x):
+        return self._model.predict(x)
     
